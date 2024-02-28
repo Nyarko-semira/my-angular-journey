168d293aace45a8741c2688d615dcb3306eed353
Natashia Semiratu Nyarko <natashiany@stlghana.com>
2024-02-28 09:19:19 +0000

    homepage,login page and signup page created and tested

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SignupPageComponent,LoginPageComponent,HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
