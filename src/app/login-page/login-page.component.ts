17d5326b958ab73718f5d8f86f18bc255e4aa137
Natashia Semiratu Nyarko <natashiany@stlghana.com>
2024-03-01 17:24:56 +0000

    added  valiadation to my signup page

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormControl, FormControlName, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
+  loginForm= new FormGroup ({
+  username :new FormControl(null,Validators.required),
+  password2 : new FormControl(null, Validators.maxLength(8)),
+  remember : new FormControl()
+
+ })
+
+ submit() {
+  if(this.loginForm.status==='VALID')
+  console.log(this.loginForm.value, this.loginForm)
+ }
 
 }
+
+
+
+
--- src/app/signup-page/signup-page.component.ts
+++ src/app/signup-page/signup-page.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './signup-page.component.html',
-  styleUrl: './signup-page.component.scss'
+  styleUrl: './signup-page.component.scss',
 })
 export class SignupPageComponent {
+  singupForm = new FormGroup({
+    firstname: new FormControl('kofi',Validators.required),
+    lastname: new FormControl('mensah',[Validators.required]),
+    email: new FormControl(null,[Validators.required,Validators.email]),
+    phonen : new FormControl(null,[Validators.minLength(10)]),
+    password: new FormControl(null,[Validators.required,Validators.minLength(8)]),
+  
 
+  
+  
+  });
+
+   submit() {
+    if(this.singupForm.status==='VALID')
+    console.log(this.singupForm.value, this.singupForm)
+   }
 }
