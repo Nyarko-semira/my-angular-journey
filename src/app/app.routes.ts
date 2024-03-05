9788e6c151a10848c3f824c934099036647768cc
Natashia Semiratu Nyarko <natashiany@stlghana.com>
2024-03-05 17:35:30 +0000

    route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { SignupPageComponent } from './signup-page/signup-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'signup', component: SignupPageComponent },
+  { path: 'login', component: LoginPageComponent },
+  {path: 'homepage', component:HomePageComponent}
+];
