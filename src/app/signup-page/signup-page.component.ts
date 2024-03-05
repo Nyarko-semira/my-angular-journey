9bd5135d2b63095e689ea81965b7929c0e2fe3a8
Natashia Semiratu Nyarko <natashiany@stlghana.com>
2024-03-05 17:12:56 +0000

    spelling correction

--- src/app/signup-page/signup-page.component.ts
+++ src/app/signup-page/signup-page.component.ts
@@ -1,19 +1,20 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-signup-page',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './signup-page.component.html',
   styleUrl: './signup-page.component.scss',
 })
 export class SignupPageComponent {
-  singupForm = new FormGroup({
+  signupForm = new FormGroup({
     firstname: new FormControl('kofi',Validators.required),
     lastname: new FormControl('mensah',[Validators.required]),
     email: new FormControl(null,[Validators.required,Validators.email]),
-    phonen : new FormControl(null,[Validators.minLength(10)]),
+    phonen : new FormControl(null,[Validators.minLength(10), Validators.maxLength(10)]),
     password: new FormControl(null,[Validators.required,Validators.minLength(8)]),
   
 
@@ -22,7 +23,7 @@ export class SignupPageComponent {
   });
 
    submit() {
-    if(this.singupForm.status==='VALID')
-    console.log(this.singupForm.value, this.singupForm)
+    if(this.signupForm.status==='VALID')
+    console.log(this.signupForm.value, this.signupForm)
    }
 }
