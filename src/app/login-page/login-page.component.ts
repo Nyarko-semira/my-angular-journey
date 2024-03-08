281e2be6f6e3ea9c6b954ba8214af44396192467
Natashia Semiratu Nyarko <natashiany@stlghana.com>
2024-03-08 10:58:06 +0000

    format code

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,36 +1,58 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
-import { FormControl, FormControlName, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import {
+  FormControl,
+  FormControlName,
+  FormGroup,
+  FormsModule,
+  NgForm,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { onErrorResumeNext } from 'rxjs';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
   imports: [FormsModule, RouterLink],
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.scss'
+  styleUrl: './login-page.component.scss',
 })
 export class LoginPageComponent {
-  http =inject(HttpClient)
-//   loginForm= new FormGroup ({
-//   username :new FormControl(null,Validators.required),
-//   password2 : new FormControl(null, Validators.maxLength(8)),
-//   remember : new FormControl()
+  http = inject(HttpClient);
+  router = inject(Router);
+  //   loginForm= new FormGroup ({
+  //   username :new FormControl(null,Validators.required),
+  //   password2 : new FormControl(null, Validators.maxLength(8)),
+  //   remember : new FormControl()
 
-//  })
-     login={username :null, password2 : null, remember:false}
- submit(form:NgForm) {
-  if(form.status==='VALID'){
-    this.http.post('https://jsonplaceholder.typicode.com/todos',form.value).subscribe((res) =>{
-      console.log(res)
-    })
+  //  })
+  login = { username: null, password2: null, remember: false };
+  submit(form: NgForm) {
+    if (form.status === 'VALID') {
+      this.http
+        .post('https://jsonplaceholder.typicode.com/todos', form.value)
+        .subscribe({
+          next: (res) => {
+            console.log(res);
+            this.router.navigateByUrl('homepage');
+          },
 
+          error: (error) => {
+            console.log(error);
+          },
+        });
+    }
   }
-  console.log(form.value, this.login)
- }
-
 }
 
+//     subscribe((res) =>{
+//       console.log(res)
+//     })
 
+//   }
+//   console.log(form.value, this.login)
+//  }
 
-
+// }
