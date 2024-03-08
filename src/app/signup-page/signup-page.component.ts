5cb8252a814da3bfca6342c3b4de4ffde8c6effd
Natashia Semiratu Nyarko <natashiany@stlghana.com>
2024-03-08 10:55:37 +0000

    inject httpclient and router

--- src/app/signup-page/signup-page.component.ts
+++ src/app/signup-page/signup-page.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { routes } from '../app.routes';
+
 
 @Component({
   selector: 'app-signup-page',
@@ -9,7 +12,10 @@ import { RouterLink } from '@angular/router';
   templateUrl: './signup-page.component.html',
   styleUrl: './signup-page.component.scss',
 })
+
 export class SignupPageComponent {
+  http = inject(HttpClient);
+  router = inject(Router);
   signupForm = new FormGroup({
     firstname: new FormControl('kofi',Validators.required),
     lastname: new FormControl('mensah',[Validators.required]),
@@ -17,13 +23,26 @@ export class SignupPageComponent {
     phonen : new FormControl(null,[Validators.minLength(10), Validators.maxLength(10)]),
     password: new FormControl(null,[Validators.required,Validators.minLength(8)]),
   
-
-  
-  
   });
+
 
    submit() {
     if(this.signupForm.status==='VALID')
     console.log(this.signupForm.value, this.signupForm)
-   }
+    this.http
+    .post('https://jsonplaceholder.typicode.com/todos', this.signupForm.value)
+    .subscribe({
+      next: (res) => {
+        console.log(res);
+        this.router.navigateByUrl('login');
+      },
+
+      error: (er) => {
+        console.log(er);
+      },
+    });
 }
+
+
+   }
+
