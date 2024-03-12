263393c5c367360b3b458201c14762ede3ed6b5f
Natashia Semiratu Nyarko <natashiany@stlghana.com>
2024-03-12 10:51:37 +0000

    imported TodoItemComponent

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { TodoItemComponent } from './todo-item/todo-item.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, TodoItemComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
