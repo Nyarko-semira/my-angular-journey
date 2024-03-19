45ffea9dcaf110b86bd9e99150da797c4b0e9071
Natashia Semiratu Nyarko <natashiany@stlghana.com>
2024-03-19 08:43:57 +0000

    struture

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,6 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { TodoItemComponent } from './todo-item/todo-item.component';
+  
+
+
+ 
+  type Todo = {
+    name : string
+    time : Date
+    type : string
+    isDone : boolean
+  }
 
 @Component({
   selector: 'app-home-page',
@@ -10,13 +20,37 @@ import { TodoItemComponent } from './todo-item/todo-item.component';
   styleUrl: './home-page.component.scss'
 })
 export class HomePageComponent {
-  date = new Date()
-  date1 = new Date(new Date().setHours(1))
-  date2 =  new Date(new Date().setHours(2))
-  date3 = new Date(new Date().setHours(3))
- date4 = new Date(new Date().setHours(4))
- date5 = new Date(new Date().setHours(5))
- date6 = new Date(new Date().setHours(6))
- date7 = new Date(new Date().setHours(7))
 
+ todoItems:Todo[] = [{
+  name : 'breakfast',
+  time : new Date(new Date().setHours(1)),
+  type : 'food',
+  isDone : true
+
+ }, 
+{
+  name : 'lunch',
+  time : new Date(new Date().setHours(4)),
+  type : 'food',
+  isDone : true
+},
+
+{
+  name : 'go to the mall',
+  time : new Date(new Date().setHours(5)),
+  type : 'errands',
+  isDone : false
+},
+
+{
+  name : 'dinner',
+  time : new Date(new Date().setHours(21)),
+  type : 'food',
+  isDone : false
+} 
+
+]
+ 
 }
+
+
